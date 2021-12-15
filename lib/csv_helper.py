309eef211ca396c43ddbae0850717caef5f7d6c9
ertyurk <ertyurk@gmail.com>
2021-12-15 21:24:49 +0300

    dir safecheck

--- lib/csv_helper.py
+++ lib/csv_helper.py
@@ -1,7 +1,10 @@
 import csv  
-
+import os.path
+from os import path
 class CsvCreator:
     def __init__(self, filename, permission):
+        if path.isdir('media') == False:
+            os.mkdir('media')
         self.filename = filename
         self.permission = permission
         self.writer = csv.writer(open(f"media/{filename}", self.permission, encoding='UTF8'))
